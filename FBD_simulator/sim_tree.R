8478dec1c063bc0b10df828e9b340b09f6d5d594
dsilvestro <silvestro.daniele@gmail.com>
2016-04-14 16:15:59 +0200

    TreeSim simulation

--- FBD_simulator/sim_tree.R
+++ FBD_simulator/sim_tree.R
@@ -1,5 +1,12 @@
-setwd("~/Dropbox-personal/Dropbox/fossilizedBM/FBD_simulator")
+setwd("~/Dropbox-personal/Dropbox/fossilizedBM/fossilBM/FBD_simulator")
 library(phytools)
 
 tree<-ladderize(pbtree(n=10, scale=10))
 write.tree(tree, file="newick2.tre")
+
+
+
+library(TreeSim)
+t =sim.bd.taxa(n=20, 1, lambda=0.1, mu=0.025, frac = 1, complete = TRUE, stochsampling = FALSE)
+plot(t[[1]])
+write.tree(t[[1]], file="newick3.tre")
